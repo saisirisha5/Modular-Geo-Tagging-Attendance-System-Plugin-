df4be973abf80c84c376bb0ef4e2a7afe018043d
saisirisha5 <prabhalasaisirisha25@gmail.com>
2026-05-06 23:05:28 +0530

    Deployment initation

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin :[
+    "http://localhost:5173/",
+    "https://modular-geo-tagging-attendance-system-plugin-fk6el17di.vercel.app/"
+  ],
+  credentials:true
+}));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 // Routes
